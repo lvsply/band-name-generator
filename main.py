4ed0e1e907cfb26955702b0a5b48557e0c149788
Brandon <33883174+lvsply@users.noreply.github.com>
2022-10-04 13:52:07 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+#This program takes two variables and combines both for band naming purposes
+print("Hello! Let's create your band name!")
+city = input("What city did you grow up in? \n")
+pet = input("What is the name of a pet? \n")
+print("Your band name should be " + city +" "+ pet)
